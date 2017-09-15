24a4c577c8cc6b52f94a1d97667b6303d6c7311b
VIROSOFT <pearlyrose3@gmail.com>
2017-09-15 16:22:32 +0100

    Initial Commit by ViroSoft

--- app/src/main/java/com/squareplaces/squareplaces/MainActivity.java
+++ app/src/main/java/com/squareplaces/squareplaces/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**HELLO FROM VIROSOFT*/
 
-    private final static int SPLASH_TIME_OUT = 2000;
+    private final static int SPLASH_TIME_OUT = 3000;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
